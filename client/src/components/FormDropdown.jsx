9b65c4b258f8af391f32c44f529caaafb4390944
Ace Logronio <64186285+aceeyou@users.noreply.github.com>
2025-08-20 08:42:13 +0800

    Update FormDropdown.jsx

--- client/src/components/FormDropdown.jsx
+++ client/src/components/FormDropdown.jsx
@@ -4,7 +4,7 @@ export default function FormDropdown({ optionList, label, formdata, setFormdata 
     return (
         <div className="input-grp">
             <label htmlFor={label}>{label}</label>
-            <select name={label} id={label} value={formdata) onChange={(e) => (setFormdata({ ...old, particulars: e.target.value })}>
+            <select name={label} id={label} value={formdata.particulars} onChange={(e) => (setFormdata({ ...old, particulars: e.target.value })}>
                 {
                     optionList.map((option, index) => (
                         <option value={option} key={index}>{option}</option>
